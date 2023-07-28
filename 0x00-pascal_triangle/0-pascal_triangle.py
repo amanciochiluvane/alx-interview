d86820a854dbd35fb02eace79f5d3afb5e90c45c
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-07-28 21:57:48 +0200

    Documented and the function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-"""
-pascal_triangle(n)== Method that returns a triangule
-"""
+# pascal_triangle.py
 
 def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the specified number of rows.
+
+    Pascal's triangle is a triangular array of integers in which the first and last elements of each row are 1,
+    and each of the other elements is the sum of the two elements immediately above it in the previous row.
+
+    Args:
+        n (int): The number of rows to generate in Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+              Each inner list corresponds to a row of the triangle, containing the elements as integers.
+
+    Example:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """
     if n <= 0:
         return []
 
