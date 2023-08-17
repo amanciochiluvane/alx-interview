efa4365f6feb646a941d60bdfa613ea92e77a25b
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-08-18 00:45:42 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,53 +2,43 @@
 
 import sys
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
 total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_code_count = {}
+
+def print_statistics():
+    print("Total file size:", total_file_size)
+    sorted_status_codes = sorted(status_code_count.keys())
+    for code in sorted_status_codes:
+        print(f"{code}: {status_code_count[code]}")
 
 try:
+    line_number = 0
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        line_number += 1
+        line = line.strip()
 
-        if len(parsed_line) > 2:
-            counter += 1
+        parts = line.split()
+        if len(parts) != 10:
+            continue
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+        ip_address = parts[0]
+        date = parts[2][1:]  # Removing '[' from the date
+        status_code = parts[8]
+        try:
+            status_code = int(status_code)
+        except ValueError:
+            continue
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+        file_size = int(parts[9])
+        total_file_size += file_size
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+        if status_code in status_code_count:
+            status_code_count[status_code] += 1
+        else:
+            status_code_count[status_code] = 1
 
-finally:
-    print_msg(dict_sc, total_file_size)
+        if line_number % 10 == 0:
+            print_statistics()
+
+except KeyboardInterrupt:
+    print_statistics()
