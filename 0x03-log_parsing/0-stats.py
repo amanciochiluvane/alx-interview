a7a4dcaf0320869d97eb6943dd81dd32658a28d3
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-08-20 18:43:46 +0200

    Update 0-stats.py last features

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 def print_msg(dict_sc, total_file_size):
     """
     Method to print
@@ -12,41 +11,38 @@ def print_msg(dict_sc, total_file_size):
     Returns:
         Nothing
     """
-
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
 
-
 total_file_size = 0
 code = 0
 counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+dict_sc = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
+        parsed_line = line.strip().split()  # Split the line into parts
+        if len(parsed_line) == 10:  # Check if the line has the correct format
             counter += 1
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+            total_file_size += int(parsed_line[-1])  # File size is the last element
+            code = parsed_line[-2]  # Status code is the second-to-last element
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+            if code in dict_sc:
+                dict_sc[code] += 1
 
-            if (counter == 10):
+            if counter == 10:
                 print_msg(dict_sc, total_file_size)
                 counter = 0
 
